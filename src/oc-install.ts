425404aa052cc20aa6c7238631f1ec49f1fd2858
Anirudh Koutha <anirudh.koutha1@bp.com>
2025-09-10 17:40:57 +0100

    fix: update Node.js version matrix in CI workflow and clean up imports

--- src/oc-install.ts
+++ src/oc-install.ts
@@ -5,7 +5,6 @@
 import * as fs from 'fs';
 import tl = require('azure-pipelines-task-lib/task');
 import path = require('path');
-
 import { ToolRunner, IExecSyncResult } from 'azure-pipelines-task-lib/toolrunner';
 import * as toolLib from 'azure-pipelines-tool-lib/tool';
 import * as semver from 'semver';
@@ -13,6 +12,7 @@ import { RunnerHandler } from './oc-exec';
 import { LINUXV3, MACOSXV3, WINV3, LINUXV4, MACOSXV4, WINV4, OC_TAR_GZ, OC_ZIP, LATEST, ZIP, TAR_GZ } from './constants';
 import { unzipArchive } from './utils/zip_helper';
 import { BinaryVersion, FindBinaryStatus } from './utils/exec_helper';
+
 const nodeFetch = (...args: Parameters<typeof import('node-fetch').default>) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 export class InstallHandler {
